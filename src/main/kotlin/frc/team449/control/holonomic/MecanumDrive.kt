fcaf86990f0297f35464ad8bc3c2a02234e84ce3
elwo-boop <79725311+elwo-boop@users.noreply.github.com>
2022-10-08 18:46:41 -0400

    Update MecanumDrive.kt

--- src/main/kotlin/frc/team449/control/holonomic/MecanumDrive.kt
+++ src/main/kotlin/frc/team449/control/holonomic/MecanumDrive.kt
@@ -5,7 +5,9 @@ import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds
 import edu.wpi.first.math.kinematics.MecanumDriveKinematics
+import edu.wpi.first.math.kinematics.MecanumDriveOdometry
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.team449.system.AHRS
 import frc.team449.system.motor.WrappedMotor
 
 class MecanumDrive(
@@ -13,14 +15,21 @@ class MecanumDrive(
   private val frontRightMotor: WrappedMotor,
   private val backLeftMotor: WrappedMotor,
   private val backRightMotor: WrappedMotor,
-  private val frontLeftLocation: Translation2d,
-  private val frontRightLocation: Translation2d,
-  private val backLeftLocation: Translation2d,
-  private val backRightLocation: Translation2d,
+  frontLeftLocation: Translation2d,
+  frontRightLocation: Translation2d,
+  backLeftLocation: Translation2d,
+  backRightLocation: Translation2d,
+  val ahrs: AHRS,
   override var pose: Pose2d,
   override val maxLinearSpeed: Double,
   override val maxRotSpeed: Double
 ): HolonomicDrive, SubsystemBase() {
+
+  init {
+    ahrs.calibrate()
+    ahrs.reset()
+  }
+
   // 10.500 x, 10.713 y (outreach 2022)
   /*
     speed = sqrt(joystickX^2+joystickY^2) * maxSpeed
@@ -33,6 +42,8 @@ class MecanumDrive(
     frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation
   )
 
+  private val odometry = MecanumDriveOdometry(this.kinematics, -ahrs.heading)
+
   private var desiredSpeeds = ChassisSpeeds()
 
   override fun set(desiredSpeeds: ChassisSpeeds) {
@@ -41,6 +52,11 @@ class MecanumDrive(
 
   override fun stop() {
     this.desiredSpeeds = ChassisSpeeds(0.0, 0.0, 0.0)
+    this.set(desiredSpeeds)
+  }
+
+  override fun periodic() {
+    val mecanumDriveModuleStates: MecanumDriveWheelSpeeds = this.kinematics.toWheelSpeeds(this.desiredSpeeds)
   }
 
 }
