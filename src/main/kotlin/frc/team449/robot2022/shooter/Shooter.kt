9b7a5e73bd792f7a453b90c2ed25fcede4171e98
Teddy <tewodros.mengistie@gmail.com>
2022-10-27 18:10:56 -0400

    Started to add PID and Feed Forward for outreach robot's shooter mechanism

--- src/main/kotlin/frc/team449/robot2022/shooter/Shooter.kt
+++ src/main/kotlin/frc/team449/robot2022/shooter/Shooter.kt
@@ -1,15 +1,21 @@
 package frc.team449.robot2022.shooter
 
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.math.controller.SimpleMotorFeedforward
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.team449.system.motor.WrappedMotor
 
 class Shooter(
+  private val feedForward: SimpleMotorFeedforward,
+  private val controller: PIDController,
+
   val shooterMotor: WrappedMotor
+
 ) : SubsystemBase() {
 
   // Run shooter motor forward
   fun runShooter() {
-    shooterMotor.set(ShooterConstants.shooterVoltage)
+    shooterMotor.setVoltage(controller.calculate(shooterMotor.velocity, ShooterConstants.desiredShooterVoltage) + feedForward.calculate(shooterMotor.velocity, ShooterConstants.desiredShooterVoltage, ShooterConstants.deltaTime))
   }
 
   // Stop shooter motor
--- src/main/kotlin/frc/team449/robot2022/shooter/ShooterConstants.kt
+++ src/main/kotlin/frc/team449/robot2022/shooter/ShooterConstants.kt
@@ -1,5 +1,6 @@
 package frc.team449.robot2022.shooter
 
 object ShooterConstants {
-  const val shooterVoltage = 0.1
+  const val desiredShooterVoltage = 0.1 // TODO - test for the values
+  const val deltaTime = 0.02; // TODO - test for the values
 }
