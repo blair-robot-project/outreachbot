75e8d9b1ce4756d5b4f2aa0d3a8609884a6e15e6
Tewodros Mengistie <Tewodros.mengistie@gmail.com>
2022-12-03 19:46:01 -0500

    fix gyro

--- src/main/kotlin/frc/team449/system/AHRS.kt
+++ src/main/kotlin/frc/team449/system/AHRS.kt
@@ -6,24 +6,22 @@ import edu.wpi.first.wpilibj.interfaces.Gyro
 import edu.wpi.first.wpilibj.simulation.SimDeviceSim
 import frc.team449.util.simBooleanProp
 import frc.team449.util.simDoubleProp
-import io.github.oblarg.oblog.annotations.Log
 
 class AHRS(private val navx: com.kauailabs.navx.frc.AHRS) : Gyro by navx {
   private var headingOffset = 0.0
 
+  /** The current reading of the gyro with the offset included */
   var heading: Rotation2d
-    @Log
     get() {
-      return Rotation2d.fromDegrees(headingOffset + this.navx.fusedHeading)
+      return -Rotation2d.fromDegrees(headingOffset + this.navx.fusedHeading)
     }
     set(newHeading) {
-      this.headingOffset = this.navx.fusedHeading - newHeading.degrees
+      this.headingOffset = newHeading.degrees - this.navx.fusedHeading
     }
 
   constructor(port: SerialPort.Port = SerialPort.Port.kMXP) : this(com.kauailabs.navx.frc.AHRS(port))
 
   override fun reset() {
-    navx.zeroYaw()
     heading = Rotation2d()
   }
 
